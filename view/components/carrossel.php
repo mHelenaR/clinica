7eeaeb07f407ca9f693add7d6ab14b4c22ab8d17
maria <mariahelenarocha54@gmail.com>
2023-06-19 16:33:55 -0300

    carrossel com imagens

---
+++ view/components/carrossel.php
@@ -0,0 +1,24 @@
+<ol class="carousel-indicators">
+    <li data-target="#meuCarrossel" data-slide-to="0" class="active"></li>
+    <li data-target="#meuCarrossel" data-slide-to="1"></li>
+    <li data-target="#meuCarrossel" data-slide-to="2"></li>
+</ol>
+<div class="carousel-inner">
+    <div class="carousel-item active">
+        <img class="d-block w-100" src="view/images/saude3.jpeg" alt="Primeiro Slide">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="view/images/saude2.jpeg" alt="Segundo Slide">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="view/images/saude1.jpeg" alt="Terceiro Slide">
+    </div>
+</div>
+<a class="carousel-control-prev" href="#meuCarrossel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" style="background-color: black;" aria-hidden="true"></span>
+    <span class="sr-only">Anterior</span>
+</a>
+<a class="carousel-control-next" href="#meuCarrossel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Próximo</span>
+</a>
--- view/login.php
+++ view/login.php
@@ -1,4 +1,80 @@
 <!DOCTYPE html>
+<html>
+
+<head>
+    <title>Bem vindo de volta...</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+
+    <style>
+        .containerForm {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .form {
+            text-align: center;
+        }
+
+        .meu-carrossel {
+            margin: 20px;
+            width: 100%;
+            border-radius: 20px;
+            overflow: hidden;
+            
+            position: relative;
+            background-color: aquamarine;
+
+        }
+
+        
+    </style>
+</head>
+
+<body>
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+
+                <div class="col-sm-12 col-md-8 d-flex justify-content-center">
+                    <div id="meuCarrossel" class="carousel slide meu-carrossel" data-ride="carousel">
+                        <?php include 'components/carrossel.php' ?>
+                    </div>
+                </div>
+
+                <div class="col-sm-12 col-md-4 d-flex justify-content-center">
+                    <div class="containerForm">
+                        <div class="form">
+                            <form method="post" action="/login">
+                                <label>Bem-vindo(a)</label><br>
+                                <label>de volta!</label><br>
+                                <input id="email" name="email" type="" placeholder="E-mail" /><br><br>
+                                <input id="senha" name="senha" type="password" placeholder="Senha" /><br>
+
+                                <button type="submit" name="btn_Entrar">Entrar</button><br>
+                                <label>Ainda não possui uma conta?</label><br>
+                                <label>CADASTRAR-SE</label><br>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
+
+
+<!---
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -21,4 +97,4 @@
     </form>
 </body>
 
-</html>
+</html> --->
