a9024764fd0c947f83831624ef54d639aefcc23e
maria <mariahelenarocha54@gmail.com>
2023-07-13 22:41:24 -0300

    botoes e input

--- view/login.php
+++ view/login.php
@@ -5,7 +5,9 @@
     <title>Bem vindo de volta...</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-
+    <link rel="stylesheet" type="text/css" href="../view/css/input.css">
+    <link rel="stylesheet" type="text/css" href="../view/css/botao.css">
+    <link rel="stylesheet" type="text/css" href="../view/css/label.css">
 
     <style>
         .containerForm {
@@ -20,16 +22,13 @@
         }
 
         .meu-carrossel {
-        
             width: 100%;
             border-radius: 20px;
             overflow: hidden;
             height: 700px;
             background-color: aquamarine;
-
         }
-
-    
+       
     </style>
 </head>
 
@@ -37,27 +36,22 @@
     <main>
         <div class="container-fluid">
             <div class="row">
-
                 <div class="col-sm-12 col-md-8 d-flex justify-content-center align-items-center">
-
                     <div id="meuCarrossel" class="carousel slide meu-carrossel" data-ride="carousel">
                         <?php include 'components/carrossel.php' ?>
                     </div>
-
                 </div>
-
                 <div class="col-sm-12 col-md-4 d-flex justify-content-center">
                     <div class="containerForm">
                         <div class="form">
                             <form method="post" action="/login">
-                                <label>Bem-vindo(a)</label><br>
-                                <label>de volta!</label><br>
-                                <input id="email" name="email" type="" placeholder="E-mail" /><br><br>
-                                <input id="senha" name="senha" type="password" placeholder="Senha" /><br>
-
-                                <button type="submit" name="btn_Entrar">Entrar</button><br>
-                                <label>Ainda não possui uma conta?</label><br>
-                                <label>CADASTRAR-SE</label><br>
+                                <label class="label-destaque">Bem-vindo(a)</label><br>
+                                <label class="label-destaque">de volta!</label><br>
+                                <input class="input-arredondado" id="email" name="email" type="" placeholder="E-mail" /><br><br>
+                                <input class="input-arredondado" id="senha" name="senha" type="password" placeholder="Senha" /><br><br>
+                                <button class="botao" type="submit" name="btn_Entrar">Entrar</button><br>
+                                <label class="label-login" style="color: #BEBEBE;">Ainda não possui uma conta?</label><br>
+                                <label class="label-login" style="color: #849CCC;">CADASTRAR-SE</label><br>
                             </form>
                         </div>
                     </div>
