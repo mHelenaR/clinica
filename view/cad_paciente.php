6346f327a0cb4ba35d5121215ed58e5686b09b3e
Maria <mariahelenarocha54@gmail.com>
2023-07-20 17:35:19 -0300

    w

--- view/cad_paciente.php
+++ view/cad_paciente.php
@@ -38,7 +38,6 @@ session_start();
             margin-bottom: 50px;
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
             display: flex;
-
             align-items: start;
         }
 
